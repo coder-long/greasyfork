0fcfcdd9a377d6aa162b06412aca365dad28b5d9
helong <helong@ciandt.com>
2024-07-11 16:39:06 +0800

    feat: do some change.

--- webhook.user.js
+++ webhook.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
-// @name         Web Hook Js逆向网站钩子
+// @name         JS逆向网站钩子
 // @namespace    http://tampermonkey.net/
-// @version      0.1.0
+// @version      0.1.1
 // @description  Js逆向网站钩子 控制台打印日志信息
 // @author       helong
-// @match      *
+// @match        *
 // @grant        none
 // @license      MIT
 // @downloadURL https://update.greasyfork.org/scripts/500274/Web%20Hook%20Js%E9%80%86%E5%90%91%E7%BD%91%E7%AB%99%E9%92%A9%E5%AD%90.user.js
