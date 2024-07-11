deda9fc95da86c683edb891bfefa6f68efebd0c1
helong <helong@ciandt.com>
2024-07-11 16:44:24 +0800

    feat: do some change.

---
+++ test.user.js
@@ -0,0 +1,12 @@
+// ==UserScript==
+// @name         test
+// @namespace    http://tampermonkey.net/
+// @version      0.1.0
+// @description  test
+// @author       helong
+// @match        *
+// @grant        none
+// @license      MIT
+// ==/UserScript==
+
+console.log('this is a message.')
